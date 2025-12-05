148ea8361c7fa5f43f0473549fa434339c1e9557
lijinglin <nextuser@163.com>
2025-12-05 18:41:37 +0800

    surpport n accounts to split sol

--- day23/programs/day23/src/lib.rs
+++ day23/programs/day23/src/lib.rs
@@ -26,6 +26,45 @@ pub mod day23 {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    /**
+    'a: 关联账户信息(account info)的生命周期
+    'b: 程序账户(program account)的生命周期
+    'c: 剩余账户(remaining accounts)的生命周期
+    'info: 整个上下文结构体的生命周期
+    */
+    pub fn split_sol<'a,'b,'c,'info> (ctx:Context<'a,'b,'c,'info,SplitSol<'info>>, amount : u64)
+                ->Result<()>{
+
+        let amount_each_gas = amount /(ctx.remaining_accounts.len() as u64);
+        let the_program = &ctx.accounts.system_program;
+        for recipient in  ctx.remaining_accounts {
+            let cpi_accounts = system_program::Transfer{
+                from : ctx.accounts.signer.to_account_info(),
+                to : recipient.to_account_info(),
+            };
+            let cpi_program = the_program.to_account_info();
+            let cpi_context = CpiContext::new(cpi_program,cpi_accounts);
+            let res = system_program::transfer(cpi_context,amount_each_gas);
+            if !res.is_ok(){
+                return err!(Errors::TransferFailed)
+            }
+        }
+        Ok(())
+    }
+}
+
+#[error_code]
+pub enum Errors{
+    #[msg("transfer failed")]
+    TransferFailed,
+}
+
+#[derive(Accounts)]
+pub struct SplitSol<'info>{
+    #[account(mut)]
+    signer:Signer<'info>,
+    system_program:Program<'info,System>
 }
 
 #[derive(Accounts)]
--- day23/tests/day23.ts
+++ day23/tests/day23.ts
@@ -2,11 +2,21 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Day23 } from "../target/types/day23";
 import {expect} from 'chai';
+
 async function requestAirdrop(conn: anchor.web3.Connection, pubKey:anchor.web3.PublicKey)
 {
      const lastBockHash = await conn.getLatestBlockhash();
      let sig = await conn.requestAirdrop(pubKey , 20000);
       await conn.confirmTransaction({signature:sig,blockhash:lastBockHash },{commitment:'confirmed'});
+}
+
+function getPublicKeyes(count : number): anchor.web3.PublicKey[]{
+    let results = [];
+    for(let  i = 0 ;i < count; ++ i) {
+        const r1 = anchor.web3.Keypair.generate().publicKey;
+        results.push(r1);
+    }
+    return results;
 }
 function getFromKeypair(){
     return anchor.getProvider().wallet.payer
@@ -47,4 +57,64 @@ describe("day23", () => {
     }
 
   });
+
+  function accountMetas(... keys :anchor.web3.PublicKey[]){
+      let result = []
+      for(let key of keys){
+          let meta = {pubkey:key, isWritable:true,isSigner:false};
+          result.push(meta);
+      }
+      return result;
+  }
+
+  async function printAccountBalance( prompt:string, ... accounts :anchor.web3.PublicKey[] )
+  {
+      console.log(prompt);
+      for(let acc of accounts) {
+          const balance = await conn.getBalance(acc) / anchor.web3.LAMPORTS_PER_SOL;
+          const addr = acc.toBase58();
+          console.log(`balance of ${addr} : ${balance} SOL`)
+      }
+  }
+
+  it("split sol 3 users", async()=>{
+      const r1 = anchor.web3.Keypair.generate().publicKey;
+      const r2 = anchor.web3.Keypair.generate().publicKey;
+      const r3 = anchor.web3.Keypair.generate().publicKey;
+      await printAccountBalance("before Split Sol",r1,r2,r3);
+      let miniRent = (await conn.getMinimumBalanceForRentExemption(0)) * 3;
+      await program.methods.splitSol(new anchor.BN(miniRent)).remainingAccounts(accountMetas(r1,r2,r3)).rpc();
+
+      await printAccountBalance("after splitSol",r1,r2,r3)
+
+  })
+
+
+
+    it("split sol n users", async()=>{
+        // const r1 = anchor.web3.Keypair.generate().publicKey;
+        // const r2 = anchor.web3.Keypair.generate().publicKey;
+        // const r3 = anchor.web3.Keypair.generate().publicKey;
+        // const r4 = anchor.web3.Keypair.generate().publicKey;
+        let keys = getPublicKeyes(5);
+        await printAccountBalance("before Split Sol",... keys);
+        const metas = accountMetas(... keys);
+        let miniRent = (await conn.getMinimumBalanceForRentExemption(0)) * metas.length;
+        await program.methods.splitSol(new anchor.BN(miniRent)).remainingAccounts(metas).rpc();
+
+        await printAccountBalance("after splitSol",...keys)
+
+    })
+
+
+    it("split sol 2 users", async()=>{
+        const r1 = anchor.web3.Keypair.generate().publicKey;
+        const r2 = anchor.web3.Keypair.generate().publicKey;
+        await printAccountBalance("before Split Sol",r1,r2);
+        let miniRent = (await conn.getMinimumBalanceForRentExemption(0)) * 3;
+        await program.methods.splitSol(new anchor.BN(miniRent)).remainingAccounts(accountMetas(r1,r2)).rpc();
+
+        await printAccountBalance("after splitSol",r1,r2)
+
+    })
 });
