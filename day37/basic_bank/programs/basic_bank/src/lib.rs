107d5fd3985c815c82fcbba79c1a785b607cc535
lijinglin <nextuser@163.com>
2025-12-17 18:56:11 +0800

    withdraw bank ok

--- day37/basic_bank/programs/basic_bank/src/lib.rs
+++ day37/basic_bank/programs/basic_bank/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer,Transfer}; 
 
 declare_id!("G4cjBGgrkYBvErabZ5b4XMvqHL56vFqB6txn9u7FR181");
 
@@ -17,13 +18,104 @@ pub mod basic_bank {
 
     pub fn create_user_account(ctx: Context<CreaeUserAccount>) -> Result<()> {
        ctx.accounts.user_account.authority = ctx.accounts.user.key();
-       ctx.accounts.user_account.amount = 0;
+       ctx.accounts.user_account.balance = 0;
        msg!("create user account: {:?} , authority: {:?}",
                 ctx.accounts.user_account.key(),
                 ctx.accounts.user.key() );
        
         Ok(())
     }
+
+    pub fn deposit(ctx:Context<Deposit>,amount:u64) -> Result<()>{ 
+        require!(amount > 0 , BankError::InvalidAmount);
+
+        let user = &ctx.accounts.user.key();
+        let bank = &ctx.accounts.bank.key();
+        let transfer_ctx = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            Transfer {
+                from: ctx.accounts.user.to_account_info(),
+                to: ctx.accounts.bank.to_account_info(),
+            },
+        );
+        transfer(transfer_ctx, amount)?;
+
+        let user_account = &mut ctx.accounts.user_account;
+        user_account.balance = user_account.balance.checked_add(amount).ok_or(BankError::Overflow)?;
+
+        let bank = &mut ctx.accounts.bank;
+        bank.total_deposit = bank.total_deposit.checked_add(amount).ok_or(BankError::Overflow)?;
+
+        msg!("deposit success:  user_account: {:?} deposit lamports: {:?}, new balance : {:?} , bank total{}",
+                user_account.key(),
+                amount,
+                user_account.balance,
+                bank.total_deposit
+                );
+        Ok(())
+
+    }
+
+    pub fn withdraw(ctx:Context<Withdraw>,amount:u64) -> Result<()>{ 
+        require!(ctx.accounts.user_account.authority == ctx.accounts.user.key(), BankError::InvalidAuthority);
+        require!(ctx.accounts.user_account.balance >= amount, BankError::InsufficientBalance);
+        require!(ctx.accounts.bank.total_deposit >= amount, BankError::InsufficientDeposit);
+
+        // let cpi_ctx = CpiContext::new(
+        //     ctx.accounts.system_program.to_account_info(),
+        //     Transfer {
+        //         from: ctx.accounts.bank.to_account_info(),
+        //         to: ctx.accounts.user.to_account_info(),
+        //     },
+        // );
+        // transfer(cpi_ctx, amount)?;
+        let bank_account = ctx.accounts.bank.to_account_info();
+        let new_deposit = bank_account.lamports().checked_sub(amount).ok_or(BankError::Overflow)?;
+
+        ** bank_account.to_account_info().try_borrow_mut_lamports()? = new_deposit;
+
+        let user_account_info = ctx.accounts.user.to_account_info();
+        let new_user_balance =  user_account_info.lamports().checked_add(amount).ok_or(BankError::Overflow)?;
+        ** user_account_info.try_borrow_mut_lamports()? = new_user_balance;
+
+        msg!("withdraw success:  user_account: {:?} withdraw lamports: {:?}, new balance : {:?} , bank total deposit: {:?}",
+                ctx.accounts.user_account.key(),
+                amount,
+                ctx.accounts.user_account.balance,
+                ctx.accounts.bank.total_deposit
+                );
+        Ok(())
+    }
+
+    
+
+
+
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub user_account: Account<'info, UseAccount>,
+    #[account(mut)]
+    bank: Account<'info, Bank>,
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum BankError {
+    #[msg("Invalid Amount")]
+    InvalidAmount,
+    #[msg("Overflow")]
+    Overflow,
+    #[msg("Invalid Authority")]
+    InvalidAuthority,
+    #[msg("Insufficient Balance")]
+    InsufficientBalance,
+    #[msg("Insufficient Deposit")]
+    InsufficientDeposit,
 }
 
 #[derive(Accounts)]
@@ -31,7 +123,8 @@ pub struct Initialize <'info> {
     #[account(
         init,
         payer=payer,
-        space=std::mem::size_of::<Bank>()+8)
+        space=std::mem::size_of::<Bank>()+8,
+    )
     ]
     pub bank :Account<'info, Bank>,
     #[account(mut)]
@@ -68,5 +161,19 @@ pub struct CreaeUserAccount<'info> {
 #[account]
 pub struct UseAccount{
     pub authority: Pubkey,
-    pub amount : u64,
+    pub balance : u64,
+}
+
+
+
+#[derive(Accounts)]
+
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub user_account: Account<'info, UseAccount>,
+    #[account(mut)]
+    bank: Account<'info, Bank>,
+    pub system_program: Program<'info, System>,
 }
--- day37/basic_bank/tests/basic_bank.ts
+++ day37/basic_bank/tests/basic_bank.ts
@@ -1,8 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { BasicBank } from "../target/types/basic_bank";
-import { Keypair, PublicKey } from "@solana/web3.js";
-import { airdropSol,confirmAndPrintTxDetails } from "./util";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import { airdropSol,confirmAndPrintTxDetails, printAccount } from "./util";
 describe("basic_bank", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -15,10 +15,15 @@ describe("basic_bank", () => {
   console.log("payer:",payer.publicKey)
   console.log("bankAccount:",bankAccount.publicKey)
   
+  const user = Keypair.generate();   
 
+  const [userAccount,_bump]  = PublicKey.findProgramAddressSync(
+      [Buffer.from("bank_account"), user.publicKey.toBuffer()],
+      program.programId
+    );
 
   it("Is initialized!", async () => {
-    await airdropSol(conn,payer.publicKey,1);
+    await airdropSol(conn,payer.publicKey,2 * LAMPORTS_PER_SOL);
     // Add your test here.
     const tx = await program.methods.initialize().accounts({
       bank: bankAccount.publicKey,
@@ -29,13 +34,6 @@ describe("basic_bank", () => {
   });
 
   it("Create user account", async () => {
-    const user = Keypair.generate();    
-
-    const [userAccount,_bump]  = PublicKey.findProgramAddressSync(
-      [Buffer.from("bank_account"), user.publicKey.toBuffer()],
-      program.programId
-    );
-
     console.log("userAccount:",userAccount)
     console.log("payer:",payer.publicKey);
     console.log("user:",user.publicKey);
@@ -47,4 +45,32 @@ describe("basic_bank", () => {
     await confirmAndPrintTxDetails(conn,tx);
     console.log("Your transaction signature", tx);
   });
+
+  it("Deposit", async () => { 
+    await airdropSol(conn,user.publicKey,2 * LAMPORTS_PER_SOL);
+    await printAccount(conn,user.publicKey,"before deposit ,user:");
+    await printAccount(conn,bankAccount.publicKey,"before  deposit bankAccount");
+    const tx = await program.methods.deposit(new anchor.BN(1* LAMPORTS_PER_SOL)).accounts({
+      user: user.publicKey,
+      userAccount: userAccount,
+      bank: bankAccount.publicKey,
+    }).signers([user]).rpc();
+    await confirmAndPrintTxDetails(conn,tx);
+    await printAccount(conn,user.publicKey,"after depoist user :");
+    await printAccount(conn,bankAccount.publicKey,"after  deposit bankAccount");
+
+  });
+
+  it("Withdraw",async ()=>{
+    await printAccount(conn,user.publicKey,"before withdraw user:");
+    await printAccount(conn,bankAccount.publicKey,"before withdraw bankAccount:");
+    const tx = await program.methods.withdraw(new anchor.BN(1* LAMPORTS_PER_SOL)).accounts({
+      user: user.publicKey,
+      userAccount: userAccount,
+      bank: bankAccount.publicKey,
+    }).signers([user]).rpc();
+    await confirmAndPrintTxDetails(conn,tx);
+    await printAccount(conn,user.publicKey,"after withdraw user:");
+    await printAccount(conn,bankAccount.publicKey,"after withdraw bankAccount:");
+  })
 });
